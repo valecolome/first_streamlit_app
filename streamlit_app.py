38c9b957420963626ed389e4d6879b737bd6d760
valecolome <47230686+valecolome@users.noreply.github.com>
2022-04-27 13:04:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title("My Mom \'s New Healthy Diner")
 
-streamlit.subtitle("Breakfast Menu")
+streamlit.header("Breakfast Menu")
 
-streamlit.body("Omega 3 & Blueberry Oatmeal")
-streamlit.body("Kale, Spinach & Rocket Smoothie")
-streamlit.body("Hard-boiled Free-Range Egg")
+streamlit.markdown("Omega 3 & Blueberry Oatmeal")
+streamlit.markdown("Kale, Spinach & Rocket Smoothie")
+streamlit.markdown("Hard-boiled Free-Range Egg")
