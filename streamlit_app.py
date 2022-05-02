89d2e7482290a55be1a7a1102514616076ea06ed
valecolome <47230686+valecolome@users.noreply.github.com>
2022-05-02 15:04:44 -0500

    Update streamlit_app.py
    add new emojis and new title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit
 
 streamlit.title("My Mom \'s New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.markdown("Omega 3 & Blueberry Oatmeal")
-streamlit.markdown("Kale, Spinach & Rocket Smoothie")
-streamlit.markdown("Hard-boiled Free-Range Egg")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
