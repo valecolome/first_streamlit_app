651e7a3841360bede4351601eb3369d92ff6dab2
valecolome <47230686+valecolome@users.noreply.github.com>
2022-05-05 15:07:41 -0500

    Update streamlit_app.py
    add fruity vice response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title("My Mom \'s New Healthy Diner")
 
@@ -17,3 +18,6 @@ fruits_selected= streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.
 fruits_to_show= my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice fruit Advice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text (fruityvice_response).json
